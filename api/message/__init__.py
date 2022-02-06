8795f0469a448aaca8fdff00450244443fd7ba06
Borko <borkod@gmail.com>
2022-02-06 14:25:16 -0500

    Added python function.

---
+++ api/message/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    return func.HttpResponse("This HTTP triggered function executed successfully.", status_code=200)
