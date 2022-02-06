a4bb1849266f65a6f1f2d0cc11e0e38f2af8c4f7
Borko <borkod@gmail.com>
2022-02-06 14:43:48 -0500

    Updated python app response.

--- api/message/__init__.py
+++ api/message/__init__.py
@@ -1,9 +1,11 @@
 import logging
-
+import json
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse("This HTTP triggered function executed successfully.", status_code=200)
+    message = json.dumps({'text': 'This HTTP triggered function executed successfully.'})
+
+    return func.HttpResponse(body=message, status_code=200)
