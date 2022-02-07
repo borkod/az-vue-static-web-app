55fb60c9868d22f6f1dcfbce501b8cc1cdc9be30
Borko <borkod@gmail.com>
2022-02-06 20:57:52 -0500

    Added Github and Twitter auth providers

--- api/message/__init__.py
+++ api/message/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import base64
 import azure.functions as func
 
 
@@ -10,11 +11,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         'text': 'This HTTP triggered function executed successfully.',
     })
 
+    principal = req.headers['x-ms-client-principal']
+    decoded = base64.b64decode(principal)
+
     logging.info("PARAMS:")
     logging.info(req.params)
     logging.info("HEADERS:")
     logging.info(list(req.headers.keys()))
     logging.info("VALUES:")
     logging.info(list(req.headers.values()))
+    logging.info("USER:")
+    logging.info(decoded)
 
     return func.HttpResponse(body=message, status_code=200)
